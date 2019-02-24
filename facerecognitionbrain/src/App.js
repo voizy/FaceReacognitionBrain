3738d2c489faa1d90f440194d1e158d174869d88
voizy <blendvoiz@gmail.com>
2019-02-24 22:53:39 +0100

    fixed security issues

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -34,8 +34,25 @@ class App extends Component {
       imageUrl: "",
       box: {},
       route: "signin",
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     };
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
   }
 
   calculateFaceLocation = data => {
@@ -69,9 +86,23 @@ class App extends Component {
         // URL
         this.state.input
       )
-      .then(response =>
-        this.displayFacebox(this.calculateFaceLocation(response))
-      )
+      .then(response => {
+          if (response){
+            fetch('http://localhost:3000/image', {
+              method: 'put',
+              headers: {'Content-Type' : 'application/json'},
+              body : JSON.stringify({
+                  id : this.state.user.id              
+              })
+            })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, {entries : count}))
+            })
+
+          }
+          this.displayFacebox(this.calculateFaceLocation(response))
+        })
       .catch(err => console.log(err));
   };
   onRouteChange = (route) => {
@@ -92,7 +123,7 @@ class App extends Component {
         (
           <div>
             <Logo />
-            <Rank />
+            <Rank name={this.state.user.name} entries={this.state.user.entries} />
             <ImageLinkForm
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit}
@@ -103,8 +134,8 @@ class App extends Component {
             />
           </div>
         ) : (route === 'signin' 
-          ?<Signin onRouteChange={this.onRouteChange} />
-         : <Register onRouteChange={this.onRouteChange} />
+          ?<Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+         : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
         )} 
       </div>
     );
